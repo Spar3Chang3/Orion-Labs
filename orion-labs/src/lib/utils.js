0ed525a438450fbd2f0f339bd12ca5d47f2ca795
Edward A <e.anderson2905@gmail.com>
2024-11-20 23:07:09 -0600

    Changes by trash

--- orion-labs/src/lib/utils.js
+++ orion-labs/src/lib/utils.js
@@ -1,12 +1,16 @@
-function openInTab(url) {
+export function openInTab(url) {
 	window.open(url, '_self');
 }
 
-function openNewTab(url) {
+export function openNewTab(url) {
 	window.open(url, '_blank');
 }
 
-let icons = {
+export const icons = {
 	sus: "/assets/sus.svg",
 
+}
+
+export const socialLinks = {
+	youtube: "https://www.youtube.com/"
 }
